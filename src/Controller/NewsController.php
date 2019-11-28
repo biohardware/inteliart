f87dc51d36986c5979de588ea8780d4a21b50680
Bence <bencekeller@macbook.lan>
2019-11-28 22:19:29 +0100

    hírek hozzáadása, szerkesztése, törlése

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -2,8 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\News;
+use App\Form\NewsType;
+use App\Repository\NewsRepository;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class NewsController extends AbstractController
 {
@@ -14,6 +21,91 @@ class NewsController extends AbstractController
     {
         return $this->render('news/index.html.twig', [
             'controller_name' => 'NewsController',
+        ]);
+    }
+
+    /**
+     * @Route("/news/add", name="news_add")
+     */
+    public function news_add(Request $request,EntityManagerInterface $entityManager,UserRepository $userRepository,UserInterface $user)
+    {
+        $news = new News();
+        //$id=$request->get('user_id');
+
+       // $user=$userRepository->find($id);
+        $newsform=$this->createForm(NewsType::class,$news);
+        $newsform->handleRequest($request);
+
+
+        if ($newsform->isSubmitted() && $newsform->isValid())
+        {
+            $userId = $user->getId();
+            $news->setUserId($userId);
+            $news->setActive(1);
+            $entityManager->persist($news);
+            $entityManager->flush();
+
+            $this->addFlash("success","Sikeres hozzáadás");
+            return $this->redirectToRoute("news");
+        }
+
+        return $this->render('news/add.html.twig', [
+            'newsform' => $newsform->createView()
+        ]);
+
+    }
+    /**
+     * @Route("/news/all", name="news_all")
+     */
+    public function all_news(UserRepository $userRepository, EntityManagerInterface $entityManager)
+    {
+        $news=$entityManager->getRepository(News::class)->findAll();
+        return $this->render('news/all.html.twig', [
+            'error' => '','news' => $news
+        ]);
+    }
+
+    /**
+     * @Route("/news/edit", name="news_edit")
+     */
+
+    public function news_edit(Request $request, UserRepository $userRepository, EntityManagerInterface $entityManager,UserInterface $user, NewsRepository $newsRepository)
+    {
+        $news_id=$request->get("new_id");
+        $news=$newsRepository->find($news_id);
+        $newsform=$this->createForm(NewsType::class,$news);
+        $newsform->handleRequest($request);
+
+
+        if ($newsform->isSubmitted() && $newsform->isValid())
+        {
+            $userId = $user->getId();
+            $news->setUserId($userId);
+            $news->setActive(1);
+            $entityManager->persist($news);
+            $entityManager->flush();
+
+            $this->addFlash("success","Sikeres hozzáadás");
+            return $this->redirectToRoute("news");
+        }
+
+        return $this->render('news/edit.html.twig', [
+            'newsform' => $newsform->createView()
+        ]);
+
+    }
+    /**
+     * @Route("/news/delete", name="news_delete")
+     */
+    public function news_delete(EntityManagerInterface $entityManager,Request $request, NewsRepository $newsRepository)
+    {
+        $news_id=$request->get("new_id");
+        $news=$newsRepository->find($news_id);
+        $entityManager->remove($news);
+        $entityManager->flush();
+        $error="";
+        return $this->render('news/all.html.twig', [
+            'error' => ''
         ]);
     }
 }
---
+++ src/Entity/News.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\NewsRepository")
+ */
+class News
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $add_date;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $active;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $user_id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getAddDate(): ?\DateTimeInterface
+    {
+        return $this->add_date;
+    }
+
+    public function setAddDate(\DateTimeInterface $add_date): self
+    {
+        $this->add_date = $add_date;
+
+        return $this;
+    }
+
+    public function getActive(): ?bool
+    {
+        return $this->active;
+    }
+
+    public function setActive(bool $active): self
+    {
+        $this->active = $active;
+
+        return $this;
+    }
+
+    public function getUserId(): ?int
+    {
+        return $this->user_id;
+    }
+
+    public function setUserId(int $user_id): self
+    {
+        $this->user_id = $user_id;
+
+        return $this;
+    }
+}
